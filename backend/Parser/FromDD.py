813bae06e73e32bf3b336612a1f086104e834ce2
Jacob Weston-Davis <u7003724@anu.edu.au>
2022-07-30 18:48:00 +1000

    Parser getting closer

--- backend/Parser/FromDD.py
+++ backend/Parser/FromDD.py
@@ -1,7 +1,8 @@
 import json
 import re
+import os
 
-f = open("backend/parser/ChampFiles/Ahri.json", 'r')
+""" f = open("backend/parser/ChampFiles/Ahri.json", 'r')
 
 ahriCore = json.loads(f.read())
 
@@ -39,7 +40,63 @@ k = open("backend/parser/OutputAhri.json", 'w')
 
 k.write(json.dumps(oup, indent=4))
 
-k.close()
+k.close() """
+
+def parseChampFromFile(input):
+
+
+    champCore = json.loads(input)
+
+    # print(ahri)
+    # print((ahri["data"])["spells"])
+    # print(ahri['data']['Ahri'].keys())
+    champ = champCore['data'][(champCore['data'].keys()[0])]
+    # print(ahriUseful['stats'])
+    # print(ahri['spells'][0].keys())
+
+    spells = champ['spells']
+
+    oup = {}
+
+    oup["name"] = champ['name']
+    oup["stats"] = champ['stats']
+
+    tempSpells = []
+    tempSpells.append({"spellName" : champ['passive']['name'], "statuses" : []})
+    for i in spells:
+        tempTT = i['tooltip']
+        statii = re.findall("<status>(.*)</status>", tempTT)
+        # print(statii)
+        tempSpells.append({"spellName" : i['name'], "statuses":statii})
+
+    oup["spells"] = tempSpells
+
+    oup["features"] = []
+    oup["damageType"] = []
+
+    # f.close()
+
+    # k = open("backend/parser/OutputAhri.json", 'w')
+
+
+    # target.write(json.dumps(oup, indent=4))
+    return(json.dumps(oup, indent=4))
+    # k.close()
+
+# Below code taken from https://stackoverflow.com/questions/10377998/how-can-i-iterate-over-files-in-a-given-directory
+directory_in_str = "backend/Parser/ChampFiles"
+directory = os.fsencode(directory_in_str) 
+
+output = open("backend/parser/OutputChamps.json", 'a')
+
+for file in os.listdir(directory):
+     filename = os.fsdecode(file)
+     if filename.endswith(".json"): 
+         # print(os.path.join(directory, filename))
+         continue
+     else:
+         continue
+
 
 #nprint(oup)
 
